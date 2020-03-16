d62c2184e9df97dd17b806a6334a0676fde5c881
JordiK <jvkouetchou@outlook.com>
2020-03-16 13:52:44 +0100

    ajout de solve

---
+++ Differences finies/src/deg1/GaussSeidel.java
@@ -0,0 +1,52 @@
+package deg1;
+
+import org.ujmp.core.DenseMatrix;
+import org.ujmp.core.Matrix;
+import org.ujmp.core.SparseMatrix;
+
+public class GaussSeidel {
+	SparseMatrix  A;
+	 Matrix b;
+	
+	public GaussSeidel(SparseMatrix A, Matrix  b) {
+		if (A == null || b == null)
+			throw new NullPointerException();
+		if (A.getRowCount() != b.getRowCount())
+			throw new IllegalArgumentException();
+		this.A = A;
+		this.b = b;
+	}
+	
+	public boolean converges() {
+		for (int i = 0; i < A.getRowCount(); i++) {
+			double diagonal = Math.abs(A.getAsDouble(i,i));
+			double tmpSum = 0;
+			for (int j = 0; j <  A.getRowCount(); j++)
+				if (i != j)
+					tmpSum += Math.abs(A.getAsDouble(i,j));
+			if (tmpSum >= diagonal)
+				return false;
+		}
+		return true;
+	}
+	
+	public Matrix solveSystem(int precision) {
+		if (!converges())
+			System.err.println("La solution ne converge pas");
+		Matrix x = DenseMatrix.Factory.zeros(A.getRowCount(),1);
+		for (int k = 0; k < precision; k++)
+			for (int i = 0; i < A.getRowCount(); i++) {
+				double x0 = 0;
+				for (int j = 0; j <A.getRowCount(); j++)
+					if (i != j)
+						x0 +=A.getAsDouble(i,j)*x.getAsDouble(j,0);
+				x.setAsDouble(((b.getAsDouble(i,0) - x0)/A.getAsDouble(i,i)), i, 0);
+			        
+			
+			}
+		
+		return x;
+	}
+
+	
+}
--- Differences finies/src/deg1/Solver.java
+++ Differences finies/src/deg1/Solver.java
@@ -76,7 +76,55 @@ public class Solver implements SolverInterface{
 
     @Override
     public Matrix resolveIterative(String fonction, double a, double b, int n) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+             Matrix f = DenseMatrix.Factory.zeros(n-1,1);
+             SparseMatrix matrix = SparseMatrix.Factory.zeros(n-1, n-1);
+             
+             //remplissage de f et de la matrice creuse
+             ArrayList<Func> listeFunc = Func.calcFonction(fonction);
+
+             for (int i = 0; i <n-1; i++){
+                 if (i == 0)
+                     f.setAsDouble(Func.calcVal(((double) i+1)/n, listeFunc)/Math.pow(n, 2) + a, i, 0);
+                 else if (i == n-2)
+                     f.setAsDouble(Func.calcVal(((double) i+1)/n, listeFunc)/Math.pow(n, 2) + b, i, 0);
+                 else
+                     f.setAsDouble(Func.calcVal(((double) i+1)/n, listeFunc)/Math.pow(n, 2), i, 0);
+                 
+                 matrix.setAsDouble(2, i,i);
+                 if (i < n-2){
+                     matrix.setAsDouble(-1, i+1,i);
+                     matrix.setAsDouble(-1, i, i+1);
+                 }
+             }
+             
+             System.out.println(matrix);
+             System.out.println(f);
+             
+             /*
+             //resolution matrix= lu x = u^-1 (l^-1 f)
+             Matrix[] luDecomp = matrix.lu();
+             //System.out.println(luDecomp[1]);
+             Matrix sol = luDecomp[1].inv().mtimes((luDecomp[0].inv().mtimes(f)));*/
+             GaussSeidel solver = new GaussSeidel(matrix, f);
+     		
+     		Matrix x = solver.solveSystem(100);
+     		
+           
+
+             return x;
+         } catch (NoSuchMethodException ex) {
+             Logger.getLogger(Solver.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (SecurityException ex) {
+             Logger.getLogger(Solver.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (IllegalAccessException ex) {
+             Logger.getLogger(Solver.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (IllegalArgumentException ex) {
+             Logger.getLogger(Solver.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (InvocationTargetException ex) {
+             Logger.getLogger(Solver.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         return null;
     }
     
 }
